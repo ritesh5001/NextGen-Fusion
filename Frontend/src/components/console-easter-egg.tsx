0168c975b1fff10545d29c603acf37edfe647cae
ritesh5001 <rgiri5001@gmail.com>
2026-04-29 01:46:00 +0530

    Add Marine Data contacts CSV file

--- Frontend/src/components/console-easter-egg.tsx
+++ Frontend/src/components/console-easter-egg.tsx
@@ -4,26 +4,7 @@ import { useEffect } from "react"
 
 export default function ConsoleEasterEgg() {
   useEffect(() => {
-    // Console Easter Egg - langsung muncul saat component mount
-    console.log(`
-
- _     _       _             _            _                           _   _
-
-| |   (_)     (_)           | |          | |                         | | (_)
-
-| |    ___   ___ _ __   __ _| |_ ___  ___| |__     ___ _ __ ___  __ _| |_ ___   _____
-
-| |   | \\ \\ / / | '_ \\ / _\` | __/ _ \\/ __| '_ \\   / __| '__/ _ \\/ _\` | __| \\ \\ / / _ \\
-
-| |___| |\\ V /| | | | | (_| | ||  __/ (__| | | | | (__| | |  __/ (_| | |_| |\\ V /  __/
-
-\\_____|_| \\_/ |_|_| |_|\\__, |\\__\\___|\\___\\_| |_|  \\___|_|  \\___|\\__, |\\__|_| \\_/ \\___|
-
-                        __/ |
-
-                       |___/
-
-`)
+    console.log('NextGen Fusion')
 
     console.log('📱 Want to work with us? Reach us at:')
 
